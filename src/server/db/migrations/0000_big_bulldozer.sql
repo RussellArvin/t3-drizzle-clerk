69edd9847257fb588918691ba62880fbffb9b9a2
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-02-23 11:46:46 +0800

    chore: generate migrations

---
+++ src/server/db/migrations/0000_big_bulldozer.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "user" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"email" text NOT NULL,
+	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"updatedAt" timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
